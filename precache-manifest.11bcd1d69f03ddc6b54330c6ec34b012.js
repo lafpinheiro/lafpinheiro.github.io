3742213029e1c87a68b94c5a2af67a767f2bd2fc
Luís Augusto Ferreti Pinheiro <laf.pinheiro@gmail.com>
2020-09-28 00:36:28 +0100

    Updates

---
+++ precache-manifest.11bcd1d69f03ddc6b54330c6ec34b012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821d4972878cc21048c339b390ec583a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a81527a194099031a2c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e2a7273976c7a35de8c",
+    "url": "/static/js/2.d5968674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5968674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a81527a194099031a2c",
+    "url": "/static/js/main.e6f8c906.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
